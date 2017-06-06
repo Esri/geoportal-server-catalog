a90347d811b593f15ed3791070c83e4261105aff
Kathleen Pate <kathleen.pate@gov.ab.ca>
2017-06-06 13:21:57 -0600

    About Page Created and added to Nav:
    -Create a new page App/main/AboutPanel.js & template
    App/main/templates/AboutPanel.html & -Add new About page to
    app/nls/Resources.js file & -Add new page to app/main/app.js & -In
    app.html add new page to the Nav <ul> tags and define link content

--- geoportal/src/main/webapp/app/main/App.js
+++ geoportal/src/main/webapp/app/main/App.js
@@ -22,9 +22,10 @@ define(["dojo/_base/declare",
         "app/etc/util",
         "app/main/SearchPanel",
         "app/main/MapPanel",
+        "app/main/AboutPanel",
         "app/content/MetadataEditor",
         "app/content/UploadMetadata"], 
-function(declare, lang, topic, appTopics, Templated, template, i18n, util, SearchPanel, MapPanel,
+function(declare, lang, topic, appTopics, Templated, template, i18n, util, SearchPanel, MapPanel, AboutPanel,
     MetadataEditor, UploadMetadata) {
 
   var oThisClass = declare([Templated], {
