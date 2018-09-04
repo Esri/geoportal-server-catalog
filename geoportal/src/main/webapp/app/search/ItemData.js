d07e32d757e03179021d104c5699c07ce4c8b80a
pete5162 <pete5162@Pete.esri.com>
2018-09-04 14:34:09 -0700

    TVA: rendering data grid.

--- geoportal/src/main/webapp/app/search/ItemData.js
+++ geoportal/src/main/webapp/app/search/ItemData.js
@@ -14,20 +14,35 @@
  */
 define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/_base/array",
+    "dojo/dom-construct",
     "dijit/Dialog",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "dojo/i18n!app/nls/resources",
-    "dojo/text!./templates/ItemData.html",
-    "dojox/form/CheckedMultiSelect"
-], function (declare, Dialog, TemplatedMixin, _WidgetsInTemplateMixin, i18n, template) {
+    "dojo/text!./templates/ItemData.html"
+], function (declare, lang, array, domConstruct, Dialog, TemplatedMixin, _WidgetsInTemplateMixin, i18n, template) {
     return declare("app.management.panels.customColumn", [Dialog, TemplatedMixin, _WidgetsInTemplateMixin], {
         templateString: template,
         i18n: i18n,
         
         postCreate: function() {
           this.inherited(arguments);
-          console.log(this);
+          array.filter(Object.keys(this.item), function(key) { return key.startsWith("src_")})
+               .filter(function(key) { return !key.startsWith("src_source_") && !key.startsWith("src_uri_")  && !key.startsWith("src_task_ref_")})
+               .map(lang.hitch(this, function(key) {return {key: key, value: this.item[key]}}))
+               .filter(function(kvp) {return kvp.value!=null && (typeof kvp.value == "string"? kvp.value.trim().length>0: true)})
+               .map(function(kvp) { return {key: kvp.key.replace(/^src_/g, "").replace(/_[^_]+$/gi,"").replace(/_+/gi," "), value: kvp.value}})
+               .forEach(lang.hitch(this, function(kvp){
+                  this._appendData(kvp.key, kvp.value);
+               }));
+        },
+        
+        _appendData: function(name, value) {
+          var row = domConstruct.create("tr", null, this.table);
+          domConstruct.create("td", {innerHTML: name}, row);
+          domConstruct.create("td", {innerHTML: value}, row);
         }
     });
 });
