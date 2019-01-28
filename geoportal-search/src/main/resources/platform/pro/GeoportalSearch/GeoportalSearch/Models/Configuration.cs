8845153c59f85cb012120277bf03f2024de95680
Kyle Perri <45472111+Kbperri@users.noreply.github.com>
2019-01-28 10:33:20 -0800

    Added portal configuration to add-in

---
+++ geoportal-search/src/main/resources/platform/pro/GeoportalSearch/GeoportalSearch/Models/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace GeoportalSearch
+{
+	internal class Configuration
+	{
+		#region Properties
+		public string name { get; set; }
+
+		public string url { get; set; }
+
+		public string type { get; set; }
+
+		public string profile { get; set; }
+
+		public string requiredFilter { get; set; }
+
+		public bool enabled { get; set; }
+
+		public bool useProxy { get; set; }
+
+		public bool disableContentType { get; set; }
+		#endregion
+	}
+}
