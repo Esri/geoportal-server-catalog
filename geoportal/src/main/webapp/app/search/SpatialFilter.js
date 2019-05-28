0c371b284fc47c9bef30dcb3ec24c5a1dd155681
pandzel <pandzel@esri.com>
2019-05-28 10:05:03 -0700

    SpatialFilter fixed.

--- geoportal/src/main/webapp/app/search/SpatialFilter.js
+++ geoportal/src/main/webapp/app/search/SpatialFilter.js
@@ -175,7 +175,9 @@ function(declare, lang, array, aspect, djQuery, on, domConstruct, domClass, domG
           if (map && params && params.item && params.item.envelope_geo) {
             outSR = map.spatialReference;
             var env = params.item.envelope_geo.coordinates;
-            geometry = new Extent(env[0][0],env[1][1],env[1][0],env[0][1],new SpatialReference(4326));
+            if (env) {
+              geometry = new Extent(env[0][0],env[1][1],env[1][0],env[0][1],new SpatialReference(4326));
+            }
           }
           if (geometry && webMercatorUtils.canProject(geometry,outSR)) {
             var projected = webMercatorUtils.project(geometry,outSR);
