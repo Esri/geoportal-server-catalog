74f9480dafa0d9ee59c6df76a51fce7d206e72fc
pandzel <pandzel@esri.com>
2019-12-10 08:34:33 -0800

    DCAT parsing hours and minutes.

--- geoportal/src/main/java/com/esri/geoportal/dcat/DcatController.java
+++ geoportal/src/main/java/com/esri/geoportal/dcat/DcatController.java
@@ -14,14 +14,18 @@
  */
 package com.esri.geoportal.dcat;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DcatController {
   
-  /** Logger. */
+  /** Logger */
   private static final Logger LOGGER = LoggerFactory.getLogger(DcatController.class);
-  
+  /** Scheduled execution service */
+  private static final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+  /** run at property */
   private String runAt;
 
   public void setRunAt(String runAt) {
@@ -30,9 +34,45 @@ public class DcatController {
 
   public void init() {
     LOGGER.info(String.format("Initializing DCAT controller to run at %s", runAt));
+    try {
+      HoursMinutes hm = HoursMinutes.parse(runAt);
+    } catch (IllegalArgumentException ex) {
+      LOGGER.error(String.format("Initialization of DCAT controller to run at %s failed", runAt), ex);
+    }
   }
   
   public void destroy() {
     LOGGER.info("Destroying DCAT conroller");
+    executorService.shutdown();
+  }
+  
+  private static final class HoursMinutes {
+    public final int hours;
+    public final int minutes;
+    
+    public HoursMinutes(int hours, int minutes) {
+      this.hours = hours;
+      this.minutes = minutes;
+    }
+    
+    public static HoursMinutes parse(String strHM) {
+      if (strHM == null) 
+        throw new IllegalArgumentException(String.format("Null hours:minutes"));
+      
+      String [] hm = strHM.split(":");
+      if (hm==null || hm.length!=2)
+        throw new IllegalArgumentException(String.format("Invalid hours:minutes (%s)", strHM));
+      
+      try {
+        return new HoursMinutes(Integer.parseInt(hm[0]), Integer.parseInt(hm[1]));
+      } catch (NumberFormatException ex) {
+        throw new IllegalArgumentException(String.format("Invalid hours:minutes (%s)", strHM), ex);
+      }
+    }
+    
+    @Override
+    public String toString() {
+      return String.format("%d:%d", hours, minutes);
+    }
   }
 }
