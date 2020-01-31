c2624e0f387eb729ba2d86357f9dbbcb19faa663
pandzel <pandzel@esri.com>
2020-01-31 13:48:40 -0800

    INSPIRE 2.0 templates for Services fixed.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/ServiceCategoryOptions.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/ServiceCategoryOptions.js
@@ -3,7 +3,8 @@ define(["dojo/_base/declare",
         "dojo/has",
         "esri/dijit/metadata/form/Option",
         "esri/dijit/metadata/form/Options",
-        "dojo/text!./templates/ServiceCategoryOptions.html"],
+        "dojo/text!./templates/ServiceCategoryOptions.html",
+        "dojo/i18n!../../../../nls/i18nInspire"],
 function(declare, lang, has, Option, Options, template, i18nInspire) {
 
   var oThisClass = declare(Options, {
