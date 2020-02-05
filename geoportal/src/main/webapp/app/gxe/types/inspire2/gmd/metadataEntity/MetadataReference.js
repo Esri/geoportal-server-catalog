a2593d125437fe91685302c7236a1139ed604250
pandzel <pandzel@esri.com>
2020-02-05 08:44:45 -0800

    INSPIRE 2.0 reference system identifier as drop down option.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/metadataEntity/MetadataReference.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/metadataEntity/MetadataReference.js
@@ -3,11 +3,14 @@ define(["dojo/_base/declare",
         "dojo/has",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Element",
+        "esri/dijit/metadata/form/InputSelectOne",
+        "esri/dijit/metadata/form/Options",
+        "esri/dijit/metadata/form/Option",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/GcoElement",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "dojo/text!./templates/MetadataReference.html"],
-function(declare, lang, has, Descriptor, Element, AbstractObject, GcoElement, ObjectReference, template) {
+function(declare, lang, has, Descriptor, Element, InputSelectOne, Options, Option, AbstractObject, GcoElement, ObjectReference, template) {
 
   var oThisClass = declare(Descriptor, {
 
