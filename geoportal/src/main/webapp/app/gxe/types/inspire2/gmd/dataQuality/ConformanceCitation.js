d39cb8dd346c63c1b99884f08c349867e97ac079
pandzel <pandzel@esri.com>
2020-02-05 12:44:44 -0800

    INSPIRE 2.0 conformance citation.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/dataQuality/ConformanceCitation.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/dataQuality/ConformanceCitation.js
@@ -3,19 +3,17 @@ define(["dojo/_base/declare",
         "dojo/has",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Element",
-        "esri/dijit/metadata/form/InputTextArea",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/GcoElement",
         "esri/dijit/metadata/form/iso/ObjectReference",
-        "./ConformanceCitation",
-        "./ConformanceDegree",
-        "dojo/text!./templates/ConformanceReport.html"],
-function(declare, lang, has, Descriptor, Element, InputTextArea, AbstractObject, GcoElement, ObjectReference,
-  ConformanceCitation, ConformanceDegree, template) {
+        "esri/dijit/metadata/types/iso/gmd/citation/CI_Date",
+        "dojo/text!./templates/ConformanceCitation.html"],
+function(declare, lang, has, Descriptor, Element, AbstractObject, GcoElement, ObjectReference, CI_Date,
+  template) {
 
   var oThisClass = declare(Descriptor, {
 
-    templateString : template
+    templateString: template
 
   });
 
