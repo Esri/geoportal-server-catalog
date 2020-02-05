d52b748c6b5bae26de486551655c64dd184b2100
pandzel <pandzel@esri.com>
2020-02-05 14:49:10 -0800

    INSPIRE 2.0 conformance citation drop down title.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/dataQuality/ConformanceCitation.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/dataQuality/ConformanceCitation.js
@@ -3,12 +3,15 @@ define(["dojo/_base/declare",
         "dojo/has",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Element",
+        "esri/dijit/metadata/form/InputSelectOne",
+        "esri/dijit/metadata/form/Options",
+        "esri/dijit/metadata/form/Option",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/GcoElement",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "esri/dijit/metadata/types/iso/gmd/citation/CI_Date",
         "dojo/text!./templates/ConformanceCitation.html"],
-function(declare, lang, has, Descriptor, Element, AbstractObject, GcoElement, ObjectReference, CI_Date,
+function(declare, lang, has, Descriptor, Element, InputSelectOne, Options, Option, AbstractObject, GcoElement, ObjectReference, CI_Date,
   template) {
 
   var oThisClass = declare(Descriptor, {
