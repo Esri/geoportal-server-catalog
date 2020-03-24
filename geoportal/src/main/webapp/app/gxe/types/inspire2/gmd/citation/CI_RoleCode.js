82a9701abaddf74558b1f20361b8583f0c751f04
pandzel <pandzel@esri.com>
2020-03-24 07:30:48 -0700

    INSPIRE 2.0: only pointOfContact role code allowed in metadata contact section

---
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/citation/CI_RoleCode.js
@@ -0,0 +1,28 @@
+define(["dojo/_base/declare",
+        "dojo/_base/lang",
+        "dojo/has",
+        "../../base/Descriptor",
+        "esri/dijit/metadata/form/InputSelectOne",
+        "esri/dijit/metadata/form/Options",
+        "esri/dijit/metadata/form/Option",
+        "esri/dijit/metadata/form/iso/CodeListAttribute",
+        "esri/dijit/metadata/form/iso/CodeListValueAttribute",
+        "esri/dijit/metadata/form/iso/CodeListElement",
+        "esri/dijit/metadata/form/iso/CodeSpaceAttribute",
+        "dojo/text!./templates/CI_RoleCode.html"],
+function(declare, lang, has, Descriptor, InputSelectOne, Options, Option, CodeListAttribute, CodeListValueAttribute,
+  CodeListElement, CodeSpaceAttribute, template) {
+
+  var oThisClass = declare(Descriptor, {
+
+    templateString: template,
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      console.log('Custom role code')
+    }
+
+  });
+
+  return oThisClass;
+});
