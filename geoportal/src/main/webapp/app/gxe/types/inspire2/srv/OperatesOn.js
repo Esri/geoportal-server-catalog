79b0e69a6c8c78763be4ab1d961d6146262d2016
pandzel <pandzel@esri.com>
2020-03-26 11:07:34 -0700

    INSPIRE 2.0: OperatesOn switch

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/OperatesOn.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/OperatesOn.js
@@ -18,15 +18,10 @@ function(declare, lang, has, topic, Descriptor, Attribute, Element, template) {
       
       this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
         var isOther = serviceType==="other"
-        this._xlinkHref.minOccurs = isOther? 1: 0
-        this._placeholder.toggleContent(isOther || this.xlinkHrefOrg.length>0, false)
-        this._placeholder.postCreate()
-        if (isOther) {
-          this.xlinkHrefOrg = this._xlinkHref.inputWidget.getInputValue().trim()
-          this._xlinkHref.inputWidget.setInputValue("https://inspire.ec.europa.eu/sites/default/files/md_2.0_datasets_example.xml")
-        } else {
-          this._xlinkHref.inputWidget.setInputValue(this.xlinkHrefOrg)
-        }
+        this._forNetwork._isOptionallyOff = isOther
+        this._forInvokable._isOptionallyOff = !isOther
+        this._forNetwork.domNode.style.display = isOther? "none": "block"
+        this._forInvokable.domNode.style.display = !isOther? "none": "block"
       })))
     }
 
