5e5d68a05a52896a307f41cf1208d8f4f73f52e5
pandzel <pandzel@esri.com>
2020-03-30 10:25:28 -0700

    INSPIRE 2.0: Distribution Info mandatory for Invokable services

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/Distribution.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/Distribution.js
@@ -1,17 +1,30 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/topic",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "esri/dijit/metadata/types/iso/gmd/distribution/DistributionFormat",
         "./TransferOptions",
         "dojo/text!./templates/Distribution.html"],
-function(declare, lang, has, Descriptor, AbstractObject, ObjectReference, DistributionFormat, TransferOptions, template) {
+function(declare, lang, has, topic, Descriptor, AbstractObject, ObjectReference, DistributionFormat, TransferOptions, template) {
 
   var oThisClass = declare(Descriptor, {
 
-    templateString : template
+    templateString : template,
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      
+      this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
+        var isOther = serviceType==="other"
+        this._forNetwork._isOptionallyOff = isOther
+        this._forInvokable._isOptionallyOff = !isOther
+        this._forNetwork.domNode.style.display = isOther? "none": "block"
+        this._forInvokable.domNode.style.display = !isOther? "none": "block"
+      })))
+    }
 
   });
 
