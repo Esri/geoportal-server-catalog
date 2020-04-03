106d2583c52e1d364599dee93a0b635f570f7be2
pandzel <pandzel@esri.com>
2020-04-03 13:17:05 -0700

    INSPIRE 2.0: Custom ConformanceReport and ConformanceCitation for services

---
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/ConformanceCitation.js
@@ -0,0 +1,24 @@
+define(["dojo/_base/declare",
+        "dojo/_base/lang",
+        "dojo/has",
+        "../../base/Descriptor",
+        "esri/dijit/metadata/form/Element",
+        "esri/dijit/metadata/form/InputSelectOne",
+        "esri/dijit/metadata/form/Options",
+        "esri/dijit/metadata/form/Option",
+        "esri/dijit/metadata/form/iso/AbstractObject",
+        "../../../../form/iso/GcoElement",
+        "esri/dijit/metadata/form/iso/ObjectReference",
+        "esri/dijit/metadata/types/iso/gmd/citation/CI_Date",
+        "dojo/text!./templates/ConformanceCitation.html"],
+function(declare, lang, has, Descriptor, Element, InputSelectOne, Options, Option, AbstractObject, GcoElement, ObjectReference, CI_Date,
+  template) {
+
+  var oThisClass = declare(Descriptor, {
+
+    templateString: template
+
+  });
+
+  return oThisClass;
+});
