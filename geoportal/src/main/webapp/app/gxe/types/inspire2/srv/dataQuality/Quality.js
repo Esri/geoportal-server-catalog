0d2b655d049634121b0a5760a416242c6855a31e
pandzel <pandzel@esri.com>
2020-04-03 14:07:11 -0700

    INSPIRE 2.0: Switch for custom ConformanceReport and ConformanceCitation for services

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/Quality.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/Quality.js
@@ -1,19 +1,35 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/topic",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Tabs",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/ObjectReference",
+        "../../gmd/dataQuality/ConformanceReport",
         "./ConformanceReport",
         "esri/dijit/metadata/types/iso/gmd/dataQuality/Lineage",
         "./Scope",
         "dojo/text!./templates/Quality.html"],
-function(declare, lang, has, Descriptor, Tabs, AbstractObject, ObjectReference, ConformanceReport, Lineage, Scope, template) {
+function(declare, lang, has, topic, Descriptor, Tabs, AbstractObject, ObjectReference, ConformanceReport, SrvConformanceReport, Lineage, Scope, template) {
 
   var oThisClass = declare(Descriptor, {
 
-    templateString : template
+    templateString : template,
+
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      console.log('SRV conformance report')
+      
+      this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
+        var isOther = serviceType==="other"
+        this._forNetwork._isOptionallyOff = isOther
+        this._forInvokable._isOptionallyOff = !isOther
+        this._forNetwork.domNode.style.display = isOther? "none": "block"
+        this._forInvokable.domNode.style.display = !isOther? "none": "block"
+      })))
+    }
 
   });
 
