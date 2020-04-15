fbbe46acaec1ea745667bffe619d0846fed08df8
pandzel <pandzel@esri.com>
2020-04-15 11:29:27 -0700

    INSPIRE 2.0: gmd.pass element default to true for invocable, interoperable and harmonised

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/ClassConformanceReport.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/ClassConformanceReport.js
@@ -1,6 +1,7 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/dom-style",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Tabs",
         "esri/dijit/metadata/form/Element",
@@ -15,13 +16,21 @@ define(["dojo/_base/declare",
         "../../gmd/dataQuality/ConformanceDegree",
         "dojo/text!./templates/ClassConformanceReport.html",
         "dojo/i18n!../../../../../nls/i18nInspire"],
-function(declare, lang, has, Descriptor, Tabs, Element, ElementChoice, InputTextArea, AbstractObject, GcoElement, ObjectReference,
+function(declare, lang, has, domStyle, Descriptor, Tabs, Element, ElementChoice, InputTextArea, AbstractObject, GcoElement, ObjectReference,
   InvocableConformanceCitation, InteroperableConformanceCitation, HarmonisedConformanceCitation, ConformanceDegree, template, i18nInspire) {
 
   var oThisClass = declare(Descriptor, {
 
     templateString : template,
-    i18nInspire: i18nInspire
+    i18nInspire: i18nInspire,
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      
+      domStyle.set(this._invocablePass.domNode, "display", "none")
+      domStyle.set(this._interoperablePass.domNode, "display", "none")
+      domStyle.set(this._harmonisedPass.domNode, "display", "none")
+    }
 
   });
 
--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/FreeTextConformanceReport.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/FreeTextConformanceReport.js
@@ -1,6 +1,7 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/dom-style",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Tabs",
         "esri/dijit/metadata/form/Element",
@@ -9,14 +10,19 @@ define(["dojo/_base/declare",
         "esri/dijit/metadata/form/iso/GcoElement",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "./FreeTextConformanceCitation",
-        "../../gmd/dataQuality/ConformanceDegree",
         "dojo/text!./templates/FreeTextConformanceReport.html"],
-function(declare, lang, has, Descriptor, Tabs, Element, InputTextArea, AbstractObject, GcoElement, ObjectReference,
-  FreeTextConformanceCitation, ConformanceDegree, template) {
+function(declare, lang, has, domStyle, Descriptor, Tabs, Element, InputTextArea, AbstractObject, GcoElement, ObjectReference,
+  FreeTextConformanceCitation, template) {
 
   var oThisClass = declare(Descriptor, {
 
-    templateString : template
+    templateString : template,
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      
+      domStyle.set(this._pass.domNode, "display", "none")
+    }
 
   });
 
