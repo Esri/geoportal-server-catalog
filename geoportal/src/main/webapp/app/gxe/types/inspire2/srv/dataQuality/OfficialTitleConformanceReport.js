900c11c24588b1456166f431212c34afe10ca156
pandzel <pandzel@esri.com>
2020-04-15 11:29:27 -0700

    INSPIRE 2.0: gmd.pass element default to true for invocable, interoperable and harmonised

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/OfficialTitleConformanceReport.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/dataQuality/OfficialTitleConformanceReport.js
@@ -1,6 +1,7 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/dom-style",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Tabs",
         "esri/dijit/metadata/form/Element",
@@ -9,14 +10,19 @@ define(["dojo/_base/declare",
         "esri/dijit/metadata/form/iso/GcoElement",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "./OfficialTitleConformanceCitation",
-        "../../gmd/dataQuality/ConformanceDegree",
         "dojo/text!./templates/OfficialTitleConformanceReport.html"],
-function(declare, lang, has, Descriptor, Tabs, Element, InputTextArea, AbstractObject, GcoElement, ObjectReference,
-  OfficialTitleConformanceCitation, ConformanceDegree, template) {
+function(declare, lang, has, domStyle, Descriptor, Tabs, Element, InputTextArea, AbstractObject, GcoElement, ObjectReference,
+  OfficialTitleConformanceCitation, template) {
 
   var oThisClass = declare(Descriptor, {
 
-    templateString : template
+    templateString : template,
+    
+    postCreate: function() {
+      this.inherited(arguments)
+      
+      domStyle.set(this._pass.domNode, "display", "none")
+    }
 
   });
 
