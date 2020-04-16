272b4998f6dc910334af2c85f98a3225ead34aa0
pandzel <pandzel@esri.com>
2020-04-16 11:30:50 -0700

    INSPIRE 2.0: Testing

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/Distribution.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/Distribution.js
@@ -17,15 +17,25 @@ function(declare, lang, has, topic, Descriptor, AbstractObject, ObjectReference,
     postCreate: function() {
       this.inherited(arguments)
       
+      this.toggle(false)
+      
       this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
         var isOther = serviceType==="other"
+        this.toggle(isOther)
+      })))
+    },
+    
+    toggle: function(isOther) {
         this._forNetwork._isOptionallyOff = isOther
         this._forInvokable._isOptionallyOff = !isOther
-        this._forNetwork._isGxeElement = !isOther
-        this._forInvokable._isGxeElement = isOther
+
+//        this._forNetwork._isGxeElement = !isOther
+//        this._forInvokable._isGxeElement = isOther
+        
+//        this._forNetwork.toggleContent(isOther, true)
+//        this._forInvokable.toggleContent(!isOther, true)
         this._forNetwork.domNode.style.display = isOther? "none": "block"
         this._forInvokable.domNode.style.display = !isOther? "none": "block"
-      })))
     }
 
   });
