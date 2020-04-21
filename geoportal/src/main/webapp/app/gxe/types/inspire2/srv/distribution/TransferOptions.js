776e6dcbbfad89ea78613cb58f32aec32dcf439b
pandzel <pandzel@esri.com>
2020-04-21 12:40:01 -0700

    INSPIRE 2.0: Using domStyle inside Quality.js, Distribution.js and TransferOptions.js.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/TransferOptions.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/distribution/TransferOptions.js
@@ -1,6 +1,7 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/dom-style",
         "dojo/topic",
         "../../base/Descriptor",
         "esri/dijit/metadata/form/Element",
@@ -12,7 +13,7 @@ define(["dojo/_base/declare",
         "esri/dijit/metadata/types/iso/gmd/citation/CI_OnlineFunctionCode",
         "dojo/text!./templates/TransferOptions.html",
         "dojo/i18n!../../../../../nls/i18nInspire"],
-function(declare, lang, has, topic, Descriptor, Element, Attribute, AbstractObject, CodeListReference, GcoElement, ObjectReference,
+function(declare, lang, has, domStyle, topic, Descriptor, Element, Attribute, AbstractObject, CodeListReference, GcoElement, ObjectReference,
   CI_OnlineFunctionCode, template, i18nInspire) {
 
   var oThisClass = declare(Descriptor, {
@@ -23,13 +24,19 @@ function(declare, lang, has, topic, Descriptor, Element, Attribute, AbstractObje
     postCreate: function() {
       this.inherited(arguments)
       
+      this.toggle(false)
+      
       this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
         var isOther = serviceType==="other"
-        this._forInvokable._isOptionallyOff = !isOther
-        this._forInvokable._isGxeElement = isOther
-        this._forInvokable.domNode.style.display = !isOther? "none": "block"
+        this.toggle(isOther)
       })))
       
+    },
+    
+    toggle: function(isOther) {
+      this._forInvokable._isOptionallyOff = !isOther
+      this._forInvokable._isGxeElement = isOther
+      domStyle.set(this._forInvokable.domNode, "display", !isOther? "none": "block")
     }
 
   });
