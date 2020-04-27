8bdad8bc2138b049bcc3511cdfb31f58ad601dcf
pandzel <pandzel@esri.com>
2020-04-27 07:47:58 -0700

    INSPIRE 2.0: toggle() method in OperatesOn component.

--- geoportal/src/main/webapp/app/gxe/types/inspire2/srv/OperatesOn.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/srv/OperatesOn.js
@@ -1,28 +1,48 @@
 define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/has",
+        "dojo/dom-style",
         "dojo/topic",
         "../base/Descriptor",
         "esri/dijit/metadata/form/Attribute",
         "esri/dijit/metadata/form/Element",
         "dojo/text!./templates/OperatesOn.html"],
-function(declare, lang, has, topic, Descriptor, Attribute, Element, template) {
+function(declare, lang, has, domStyle, topic, Descriptor, Attribute, Element, template) {
 
   var oThisClass = declare(Descriptor, {
 
     templateString: template,
-    xlinkHrefOrg: "",
+    storedNetworkOptionallyOff: true,
     
     postCreate: function() {
       this.inherited(arguments)
       
+      this.toggle(false)
+      
       this.own(topic.subscribe("inspire/service-type-changed", lang.hitch(this, function(serviceType) {
         var isOther = serviceType==="other"
-        this._forNetwork._isOptionallyOff = isOther
-        this._forInvokable._isOptionallyOff = !isOther
-        this._forNetwork.domNode.style.display = isOther? "none": "block"
-        this._forInvokable.domNode.style.display = !isOther? "none": "block"
+        this.toggle(isOther)
       })))
+    },
+    
+    toggle: function(isOther) {
+        if (isOther) {
+          this.storedNetworkOptionallyOff = this._forNetwork._isOptionallyOff
+          this._forNetwork.toggleContent(false, false)
+          this._forNetwork._isOptionallyOff = true
+        } else {
+          this._forNetwork.toggleContent(!this.storedNetworkOptionallyOff, false)
+          this._forNetwork._isOptionallyOff = this.storedNetworkOptionallyOff
+        }
+        
+        this._forNetwork._isGxeElement = !isOther
+        domStyle.set(this._forNetwork.domNode, "display", isOther? "none": "block")
+        
+
+        this._forInvokable.toggleContent(isOther, false)
+        this._forInvokable._isGxeElement = isOther
+        this._forInvokable._isOptionallyOff = !isOther
+        domStyle.set(this._forInvokable.domNode, "display", !isOther? "none": "block")
     }
 
   });
