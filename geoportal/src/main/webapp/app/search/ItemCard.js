51b9e9b1c91eee5f99ffc8cff8f242c195e59c1e
pandzel <pandzel@esri.com>
2020-09-23 13:41:19 -0700

    ItemCard fixed.

--- geoportal/src/main/webapp/app/search/ItemCard.js
+++ geoportal/src/main/webapp/app/search/ItemCard.js
@@ -560,19 +560,19 @@ function(declare, lang, array, string, topic, xhr, on, appTopics, domClass, domC
       }
 
       // set individual fields
-      if (owner.length > 0) {
+      if (owner && owner.length > 0) {
         util.setNodeText(this.ownerNode, owner);
       } else {
         util.setNodeText(this.ownerNode, i18n.item.notAvailable);
       }
 
-      if (date.length > 0) {
+      if (date && date.length > 0) {
         util.setNodeText(this.dateNode, date);
       } else {
         util.setNodeText(this.dateNode, i18n.item.notAvailable);
       }
 
-      if (permissions.length > 0) {
+      if (permissions && permissions.length > 0) {
         util.setNodeText(this.permissionsNode, permissions);
       } else {
         util.setNodeText(this.permissionsNode, i18n.item.notAvailable);
