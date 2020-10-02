e2ca46150c38407434f894b7b92b6bbee56bd408
pandzel <pandzel@esri.com>
2020-10-02 09:23:28 -0700

    CAT: Tree widget hooked with mockup data.

--- geoportal/src/main/webapp/app/search/HierarchyTree.js
+++ geoportal/src/main/webapp/app/search/HierarchyTree.js
@@ -16,6 +16,8 @@ define(["dojo/_base/declare",
         "dojo/_base/lang",
         "dojo/_base/array",
         "dojo/dom-construct",
+        "dojo/topic",
+        "app/context/app-topics",
         "dojo/store/Memory",
         "dijit/tree/ObjectStoreModel", 
         "dijit/Tree",
@@ -25,7 +27,7 @@ define(["dojo/_base/declare",
         "app/search/DropPane",
         "app/search/QClause",
         "app/search/HierarchyTreeSettings"], 
-function(declare, lang, array, domConstruct, Memory, ObjectStoreModel, Tree, template, i18n, SearchComponent, 
+function(declare, lang, array, domConstruct, topic, appTopics, Memory, ObjectStoreModel, Tree, template, i18n, SearchComponent, 
   DropPane, QClause, HierarchyTreeSettings) {
   
   var oThisClass = declare([SearchComponent], {
@@ -40,6 +42,54 @@ function(declare, lang, array, domConstruct, Memory, ObjectStoreModel, Tree, tem
     props: null,
     
     _initialSettings: null,
+    
+    // tree structures
+    store: null,
+    model: null,
+    tree: null,
+    
+    constructor: function() {
+      this.inherited(arguments);
+      
+      this.store = new Memory({
+          data: [
+              { id: 'world', name:'The earth', type:'planet', population: '6 billion'},
+              { id: 'AF', name:'Africa', type:'continent', population:'900 million', area: '30,221,532 sq km',
+                      timezone: '-1 UTC to +4 UTC', parent: 'world'},
+                  { id: 'EG', name:'Egypt', type:'country', parent: 'AF' },
+                  { id: 'KE', name:'Kenya', type:'country', parent: 'AF' },
+                      { id: 'Nairobi', name:'Nairobi', type:'city', parent: 'KE' },
+                      { id: 'Mombasa', name:'Mombasa', type:'city', parent: 'KE' },
+                  { id: 'SD', name:'Sudan', type:'country', parent: 'AF' },
+                      { id: 'Khartoum', name:'Khartoum', type:'city', parent: 'SD' },
+              { id: 'AS', name:'Asia', type:'continent', parent: 'world' },
+                  { id: 'CN', name:'China', type:'country', parent: 'AS' },
+                  { id: 'IN', name:'India', type:'country', parent: 'AS' },
+                  { id: 'RU', name:'Russia', type:'country', parent: 'AS' },
+                  { id: 'MN', name:'Mongolia', type:'country', parent: 'AS' },
+              { id: 'OC', name:'Oceania', type:'continent', population:'21 million', parent: 'world'},
+              { id: 'EU', name:'Europe', type:'continent', parent: 'world' },
+                  { id: 'DE', name:'Germany', type:'country', parent: 'EU' },
+                  { id: 'FR', name:'France', type:'country', parent: 'EU' },
+                  { id: 'ES', name:'Spain', type:'country', parent: 'EU' },
+                  { id: 'IT', name:'Italy', type:'country', parent: 'EU' },
+              { id: 'NA', name:'North America', type:'continent', parent: 'world' },
+              { id: 'SA', name:'South America', type:'continent', parent: 'world' }
+          ],
+          getChildren: function(object){
+              return this.query({parent: object.id});
+          }
+      });
+      
+      this.model = new ObjectStoreModel({
+          store: this.store,
+          query: {id: 'world'}
+      });
+      
+      this.tree =  new Tree({
+          model: this.model
+      });
+    },
     
     postCreate: function() {
       this.inherited(arguments);
@@ -66,6 +116,11 @@ function(declare, lang, array, domConstruct, Memory, ObjectStoreModel, Tree, tem
           d.showDialog();
         });
       }
+      
+      this.own(topic.subscribe(appTopics.AppStarted, lang.hitch(this, function() {
+        this.tree.placeAt(this.entriesNode);
+        this.tree.startup();
+      })));
     },
     
     postMixInProperties: function() {
@@ -76,31 +131,31 @@ function(declare, lang, array, domConstruct, Memory, ObjectStoreModel, Tree, tem
     },
     
     addEntry: function(term,count,missingVal) {
-      var name = this.chkName(term);
-      var v = name+" ("+count+")";
-      var tipPattern = i18n.search.appliedFilters.tipPattern;
-      var tip = tipPattern.replace("{type}",this.label).replace("{value}",name);
-      var query = {"term": {}};
-      query.term[this.field] = term;
-      if (typeof missingVal === "string" && missingVal.length > 0 && missingVal === term) {
-        query = {"bool": {"must_not": {"exists": {"field": this.field}}}};
-      }
-      var qClause = new QClause({
-        label: name,
-        tip: tip,
-        parentQComponent: this,
-        removable: true,
-        scorable: true,
-        query: query
-      });
-      var nd = domConstruct.create("div",{},this.entriesNode);
-      var link = domConstruct.create("a",{
-        href: "#",
-        onclick: lang.hitch(this,function() {
-          this.pushQClause(qClause,true);
-        })
-      },nd);
-      this.setNodeText(link,v);
+//      var name = this.chkName(term);
+//      var v = name+" ("+count+")";
+//      var tipPattern = i18n.search.appliedFilters.tipPattern;
+//      var tip = tipPattern.replace("{type}",this.label).replace("{value}",name);
+//      var query = {"term": {}};
+//      query.term[this.field] = term;
+//      if (typeof missingVal === "string" && missingVal.length > 0 && missingVal === term) {
+//        query = {"bool": {"must_not": {"exists": {"field": this.field}}}};
+//      }
+//      var qClause = new QClause({
+//        label: name,
+//        tip: tip,
+//        parentQComponent: this,
+//        removable: true,
+//        scorable: true,
+//        query: query
+//      });
+//      var nd = domConstruct.create("div",{},this.entriesNode);
+//      var link = domConstruct.create("a",{
+//        href: "#",
+//        onclick: lang.hitch(this,function() {
+//          this.pushQClause(qClause,true);
+//        })
+//      },nd);
+//      this.setNodeText(link,v);
     },
     
     chkName: function(term){
