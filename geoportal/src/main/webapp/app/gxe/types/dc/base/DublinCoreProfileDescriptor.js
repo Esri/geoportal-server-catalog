c6fe023205aabe9806aaf415e16c530db6f52796
pandzel <pandzel@esri.com>
2021-05-06 07:47:13 -0700

    Dublin Core initial implementation

---
+++ geoportal/src/main/webapp/app/gxe/types/dc/base/DublinCoreProfileDescriptor.js
@@ -0,0 +1,18 @@
+define(["dojo/_base/declare", 
+        "dojo/_base/lang",
+        "esri/dijit/metadata/base/Descriptor",
+        "dojo/i18n!esri/dijit/metadata/nls/i18nBase",
+        "dojo/i18n!esri/dijit/metadata/nls/i18nIso",
+        "dojo/i18n!../nls/i18nDublinCore"],
+function(declare, lang, Descriptor, i18nBase, i18nIso, i18nDublinCore) {
+
+  var oThisClass = declare(Descriptor, {
+    
+    i18nBase: i18nBase,
+    i18nIso: i18nIso,
+    i18nDublinCore: i18nDublinCore
+    
+  });
+
+  return oThisClass;
+});
