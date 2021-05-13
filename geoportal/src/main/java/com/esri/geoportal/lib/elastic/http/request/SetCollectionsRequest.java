e6725710f91ad8b789070aece97513b5bfe9878d
pandzel <pandzel@esri.com>
2021-05-13 07:51:40 -0700

    Collections persistence.

--- geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/SetCollectionsRequest.java
+++ geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/SetCollectionsRequest.java
@@ -18,6 +18,7 @@ import com.esri.geoportal.context.AppResponse;
 import com.esri.geoportal.context.GeoportalContext;
 import com.esri.geoportal.lib.elastic.request.BulkEditRequest;
 import com.esri.geoportal.lib.elastic.util.FieldNames;
+import java.util.Arrays;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
@@ -36,76 +37,35 @@ public class SetCollectionsRequest extends BulkEditRequest {
   
   @Override
   public AppResponse execute() throws Exception {
-//    /*
-//    http://localhost:8080/geoportal/rest/metadata/setAccess?id=68e65338e166458d8425775114487b31&access=private&group=g1&group=g2
-//    
-//    access=&group=&group=
-//    */
-//    
-//    setAdminOnly(false);
-//    setProcessMessage("SetAccess");
-//    AppResponse response = new AppResponse();
-//    if (!GeoportalContext.getInstance().getSupportsGroupBasedAccess()) {
-//      String msg = "Not implemented";
-//      response.writeNotImplemented(this,JsonUtil.newErrorResponse(msg,getPretty()));
-//      return response;
-//    }
-//    
-//    boolean hasGroups = false;
-//    String[] groups = getParameterValues("group");
-//    if (groups != null && groups.length == 1) {
-//      // TODO check comma delimited?
-//    }
-//    JsonArrayBuilder jsaGroups = Json.createArrayBuilder();
-//    if (groups != null) {
-//      for (String group: groups) {
-//        if (group != null) group = group.trim();
-//        if (group != null && group.length() > 0) {
-//          jsaGroups.add(group);
-//          hasGroups = true;
-//        }
-//      }         
-//    }
-//    
-//    String access = getParameter("access");
-//    if (access != null) access = access.trim();
-//    if (access == null || access.length() == 0) {
-//      if (hasGroups) {
-//        access = "private";
-//      } else {
-//        response.writeMissingParameter(this,"access");
-//        return response;
-//      }
-//    }
-//    access = access.toLowerCase();
-//    if (!access.equals("public") && !access.equals("private")) {
-//      String msg = "access must be public or private";
-//      response.writeBadRequest(this,JsonUtil.newErrorResponse(msg,getPretty()));
-//      return response;
-//    }
-//    /*
-//    // TODO should this be enforced? 
-//    if (access.equals("public") && hasGroups) {
-//      String msg = "access must be private when groups are specified";
-//      response.writeBadRequest(this,JsonUtil.newErrorResponse(msg,getPretty()));
-//      return response;
-//    }
-//    */
-//    
-//    JsonObjectBuilder jso = Json.createObjectBuilder();
-//    jso.add(FieldNames.FIELD_SYS_ACCESS,access);
-//    if (hasGroups) {
-//      jso.add(FieldNames.FIELD_SYS_ACCESS_GROUPS,jsaGroups);
-//    } else {
-//      // TODO? is null or empty array better?
-//      jso.addNull(FieldNames.FIELD_SYS_ACCESS_GROUPS);
-//    }
-//    
-//    //jso.add(FieldNames.FIELD_SYS_MODIFIED,DateUtil.nowAsString()); // TODO should this be set?
-//    setUpdateSource(jso.build().toString());
-//    
-//    //System.err.println("updateSource="+this.getUpdateSource());
-//    //if (true) throw new RuntimeException("SetAccessRequest: temporary stop");
+    /*
+    http://localhost:8080/geoportal/rest/metadata/setCollections?id=68e65338e166458d8425775114487b31&collections=collection1,collection2
+    
+    collections=
+    */
+    
+    setAdminOnly(false);
+    setProcessMessage("SetCollections");
+    AppResponse response = new AppResponse();
+    if (!GeoportalContext.getInstance().getSupportsCollections()) {
+      String msg = "Not implemented";
+      response.writeNotImplemented(this,JsonUtil.newErrorResponse(msg,getPretty()));
+      return response;
+    }
+    
+    JsonObjectBuilder jso = Json.createObjectBuilder();
+    
+    String collectionsStr = getParameter("collections");
+    if (collectionsStr != null) {
+      collectionsStr = collectionsStr.trim();
+      String [] collections = collectionsStr.split(",");
+      
+      JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+      Arrays.stream(collections).forEach(coll -> arrBuilder.add(coll));
+      
+      jso.add(FieldNames.FIELD_SYS_COLLECTIONS,arrBuilder);
+    }
+    
+    setUpdateSource(jso.build().toString());
     return super.execute();
   }
   
