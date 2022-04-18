7e55eca3fd52fa17b5d1f4becb02d0c72cdee899
Marten <mhogeweg@esri.com>
2022-04-18 15:36:07 -0700

    clean up MD_Format
    removed unused reference

--- geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/distribution/MD_Format.js
+++ geoportal/src/main/webapp/app/gxe/types/inspire2/gmd/distribution/MD_Format.js
@@ -5,9 +5,8 @@ define(["dojo/_base/declare",
         "esri/dijit/metadata/form/iso/AbstractObject",
         "esri/dijit/metadata/form/iso/ObjectReference",
         "./formatDistributor",
-        "./TransferOptions",
         "dojo/text!./templates/MD_Format.html"],
-function(declare, lang, has, Descriptor, AbstractObject, ObjectReference, formatDistributor, TransferOptions, template) {
+function(declare, lang, has, Descriptor, AbstractObject, ObjectReference, formatDistributor, template) {
 
   var oThisClass = declare(Descriptor, {
 
