957f2af53777825e26dcfcba83be0df3f96cceef
as0050629 <ankita_srivastava@esri.com>
2022-09-07 13:27:38 +0530

    Fixed item counts

--- geoportal/src/main/webapp/app/search/Paging.js
+++ geoportal/src/main/webapp/app/search/Paging.js
@@ -134,6 +134,7 @@ function(declare, lang, on, domClass, domAttr, djNumber, topic, string, appTopic
     _renderCount: function() {
       var nHits = this.numHits? this.numHits.value? this.numHits.value: typeof(this.numHits)==="number"? this.numHits: 0: 0;
       var sType = this.typePlural;
+      nHits = nHits -1;
       if (nHits === 1) sType = this.typeSingular;
       var s = i18n.search.resultCount.countPattern;
       s = s.replace("{count}",""+djNumber.format(nHits,{}));
