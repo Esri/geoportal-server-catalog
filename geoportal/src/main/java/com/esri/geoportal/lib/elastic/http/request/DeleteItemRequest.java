da3e4f908ea9f5f9fae42cece462f2d76c24e12a
Ankita Srivastava <ankita_srivastava@esri.com>
2023-09-29 20:04:49 +0530

    Uncomment DeleteItemRequest execute method
    This is called from Harvester to perform cleanup

--- geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/DeleteItemRequest.java
+++ geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/DeleteItemRequest.java
@@ -62,46 +62,46 @@ public class DeleteItemRequest extends BulkEditRequest {
   @Override
   public AppResponse execute() throws Exception {    
     AppResponse response = new AppResponse();
-//    String id = getId();
-//    if (id == null || id.length() == 0) {
-//      response.writeIdIsMissing(this);
-//      return response;
-//    }
-//    AccessUtil au = new AccessUtil();
-//    id = au.determineId(id); // TODO
-//    au.ensureWriteAccess(getUser(),id); // TODO
-//    
-//    ElasticContext ec = GeoportalContext.getInstance().getElasticContext();
-//    ElasticClient client = ElasticClient.newClient();
-//    String url = client.getItemUrl(ec.getIndexName(),ec.getActualItemIndexType(),id);
-//    boolean useSeparateXmlItem = ec.getUseSeparateXmlItem();
-//    boolean deletingXml = false;
-//    
-//    try {
-//      String result = client.sendDelete(url);
-//      if (checkResult(result)) {
-//        if (useSeparateXmlItem) {
-//          deletingXml = true;
-//          String url2 = client.getXmlUrl(ec.getIndexName(),ec.getXmlIndexType(),id);
-//          String result2 = client.sendDelete(url2);
-//          if (checkResult(result2)) {
-//            this.writeOk(response,id);
-//          } else {
-//            throw new Exception("Error deleting item.");
-//          }
-//        } else {
-//          this.writeOk(response,id);
-//        }
-//      } else {
-//        throw new Exception("Error deleting XML for item.");
-//      }
-//    } catch (FileNotFoundException e) {
-//      if (deletingXml) {
-//        this.writeOk(response,id);
-//      } else {
-//        response.writeIdNotFound(this,id);
-//      }
-//    }
+   String id = getId();
+   if (id == null || id.length() == 0) {
+     response.writeIdIsMissing(this);
+     return response;
+   }
+   AccessUtil au = new AccessUtil();
+   id = au.determineId(id); // TODO
+   au.ensureWriteAccess(getUser(),id); // TODO
+   
+   ElasticContext ec = GeoportalContext.getInstance().getElasticContext();
+   ElasticClient client = ElasticClient.newClient();
+   String url = client.getItemUrl(ec.getIndexName(),ec.getActualItemIndexType(),id);
+   boolean useSeparateXmlItem = ec.getUseSeparateXmlItem();
+   boolean deletingXml = false;
+   
+   try {
+     String result = client.sendDelete(url);
+     if (checkResult(result)) {
+       if (useSeparateXmlItem) {
+         deletingXml = true;
+         String url2 = client.getXmlUrl(ec.getIndexName(),ec.getXmlIndexType(),id);
+         String result2 = client.sendDelete(url2);
+         if (checkResult(result2)) {
+           this.writeOk(response,id);
+         } else {
+           throw new Exception("Error deleting item.");
+         }
+       } else {
+         this.writeOk(response,id);
+       }
+     } else {
+       throw new Exception("Error deleting XML for item.");
+     }
+   } catch (FileNotFoundException e) {
+     if (deletingXml) {
+       this.writeOk(response,id);
+     } else {
+       response.writeIdNotFound(this,id);
+     }
+   }
     return response;
   }
   
