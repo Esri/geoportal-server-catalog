a534243cde7217c2ac8d39e757c36f00dfcbb558
cont_anki <ankita_srivastava@esri.com>
2024-04-03 16:01:35 +0530

    Added feature limit and error detail in POST

--- geoportal/src/main/java/com/esri/geoportal/context/GeoportalContext.java
+++ geoportal/src/main/java/com/esri/geoportal/context/GeoportalContext.java
@@ -52,9 +52,10 @@ public class GeoportalContext implements ApplicationContextAware {
   private HarvesterContext harvesterContext;
   private boolean supportsApprovalStatus = false;
   private boolean supportsGroupBasedAccess = false;
-  private String version = "2.6.0";
+  private String version = "2.7.2";
   private boolean parseGml;
   private boolean supportsCollections = false;
+  private int numFeaturesStacAddItem = 100;
   
   /** Constructor */
   public GeoportalContext() {}
@@ -223,5 +224,16 @@ public class GeoportalContext implements ApplicationContextAware {
   public void setParseGml(boolean parseGml) {
     this.parseGml = parseGml;
   }
+
+	/**
+	 * @return number of features allowed in FeatureCollection for Stac Transaction POST request
+	 */
+	public int getNumFeaturesStacAddItem() {
+		return numFeaturesStacAddItem;
+	}
+	
+	public void setNumFeaturesStacAddItem(int numFeaturesStacAddItem) {
+		this.numFeaturesStacAddItem = numFeaturesStacAddItem;
+	}
 
 }
