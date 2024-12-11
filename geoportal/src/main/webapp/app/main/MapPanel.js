eaefada4ce97db9f7408904dde3ce7078ccb4d51
cont_anki <ankita_srivastava@esri.com>
2024-12-11 11:43:19 +0530

    Esri 4 for Map panel

--- geoportal/src/main/webapp/app/main/MapPanel.js
+++ geoportal/src/main/webapp/app/main/MapPanel.js
@@ -19,8 +19,21 @@ define(["dojo/_base/declare",
         "dojo/i18n!../nls/resources",
         "dojo/sniff",
         "dojo/dom-style",
-        "dojo/dom-geometry"], 
-function(declare, lang, Templated, template, i18n, has, domStyle, domGeometry) {
+        "dojo/dom-geometry",
+        "esri4/Map",
+        "esri4/views/MapView",
+        "esri4/layers/TileLayer",
+        "esri4/layers/MapImageLayer",
+        "esri4/widgets/Search",
+        "esri4/widgets/LayerList",
+        "esri4/widgets/FeatureTable",
+        "esri4/widgets/Legend",
+        "esri4/widgets/Locate",
+        "esri4/Graphic"/*,
+        "../gs/widget/SearchPane",
+        "../gs/widget/WidgetContext"*/], 
+function(declare, lang, Templated, template, i18n, has, domStyle, domGeometry,
+		Map,MapView,TileLayer, MapImageLayer,SearchWidget,LayerList,FeatureTable,Legend,Locate,Graphic /*,SearchPane,WidgetContext*/) {
 
   var oThisClass = declare([Templated], {
 
@@ -34,32 +47,119 @@ function(declare, lang, Templated, template, i18n, has, domStyle, domGeometry) {
     },
     
     addToMap: function(params) {
-      var mapWindow  = this.mapFrameNode.contentWindow;
+      var mapWindow  = this.mapNode.contentWindow;
       if (mapWindow && typeof mapWindow.addToMapListener === "function") {
         mapWindow.addToMapListener(params);
       }
     },
-  
+  //Check Widget.js for opening map panel
     ensureMap: function(urlParams) {
-      var url = "./viewer/index.html"; // TODO config this?
-      if (!this.mapWasInitialized) {
-        this.mapWasInitialized = true;
-        if (urlParams) {
-          var sProp = null, oProp = null, props = urlParams;
-          for (sProp in props) {
-            if (props.hasOwnProperty(sProp)) {
-              oProp = props[sProp];
-              if (typeof oProp !== "undefined" && oProp !== null) {
-                if (url.indexOf("?") === -1) url += "?";
-                else url += "&";
-                url += sProp+"="+encodeURIComponent(oProp);
-              }
-            }
-          }
-        }
-        //console.warn("viewerUrl",url);
-        this.mapFrameNode.src = url;
-      }
+		var mapProps = AppContext.appConfig.searchMap || {};
+	    if (mapProps) mapProps = lang.clone(mapProps);
+	    var v = mapProps.basemapUrl;
+	    delete mapProps.basemapUrl;
+	    if (typeof mapProps.basemap === "string" && mapProps.basemap.length > 0) {
+	      v = null;
+	    }
+	    
+	    var map = new Map({basemap:"streets"});
+	    
+		const view = new MapView({
+	  	  container: this.mapNode,
+	  	  map: map, 
+	  	  center: mapProps.center,
+	  	  zoom: mapProps.zoom      	  
+	  	});
+		this.view = view;
+		
+		 if (typeof v === "string" && v.length > 0) {
+		        v =  util.checkMixedContent(v);
+	        var basemap;
+	        if (!mapProps.isTiled) {
+	          basemap = new MapImageLayer(v);
+	        } else {
+	          basemap = new TileLayer(v);
+	        }
+	        map.add(basemap);
+	      }
+		      
+	
+	      this.view.when(lang.hitch(this,function() { 
+	    	  const searchWidget = new SearchWidget({
+	    		  view: this.view
+	    		});
+	    	  this.view.ui.add(searchWidget, {
+	    		  position: "top-left",
+	    		  index: 0
+	    		}); 
+	    	  const layerList = new LayerList({
+	    		  view: view
+	    		});
+	    		// Adds widget below other elements in the top left corner of the view
+	    		view.ui.add(layerList, {
+	    		  position: "top-left"
+	    		});
+	    		
+	    		let locateWidget = new Locate({
+	    			  view: view,   // Attaches the Locate button to the view
+	    			  graphic: new Graphic({
+	    			    symbol: { type: "simple-marker" }  // overwrites the default symbol used for the
+	    			    // graphic placed at the location of the user when found
+	    			  })
+	    			});
+	    		view.ui.add(locateWidget, "top-left");
+	    		
+	    		let legend = new Legend({
+	    			  view: view
+	    		});
+
+	    		view.ui.add(legend, "bottom-right");
+	    	
+	    	  
+	     }))    	
+    	
+    	//Try this for Geoportal Search in expander (geoportal-search - widget.js
+//        var localGeoportalUrl = this._createLocalCatalogUrl();
+//        if (localGeoportalUrl) {
+//          array.forEach(this.config.targets, function(target){
+//            if (target.type === "geoportal" && !target.url) {
+//              target.url = localGeoportalUrl;
+//            }
+//          });
+//        }
+//        var widgetContext = new WidgetContext({
+//          i18n: i18n,
+//          map: this.map,
+//          proxyUrl: esriConfig.defaults.io.proxyUrl,
+//          wabWidget: this,
+//          widgetConfig: this.config
+//        });
+//        var searchPane = new SearchPane({
+//          i18n: widgetContext.i18n,
+//          widgetContext: widgetContext
+//        },this.widgetNode);
+//        searchPane.startup();
+    	
+    	
+//    	var url = "./viewer/index.html"; // TODO config this?
+//      if (!this.mapWasInitialized) {
+//        this.mapWasInitialized = true;
+//        if (urlParams) {
+//          var sProp = null, oProp = null, props = urlParams;
+//          for (sProp in props) {
+//            if (props.hasOwnProperty(sProp)) {
+//              oProp = props[sProp];
+//              if (typeof oProp !== "undefined" && oProp !== null) {
+//                if (url.indexOf("?") === -1) url += "?";
+//                else url += "&";
+//                url += sProp+"="+encodeURIComponent(oProp);
+//              }
+//            }
+//          }
+//        }
+//        //console.warn("viewerUrl",url);
+//        this.mapFrameNode.src = url;
+//      }
     }
 
   });
