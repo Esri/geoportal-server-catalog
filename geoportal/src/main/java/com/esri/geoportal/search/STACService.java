9c1b00f8464eb04cb728b3f9ea4d7e2daf758aff
cont_anki <ankita_srivastava@esri.com>
2025-03-25 15:16:44 +0530

    Increased Sleep time to 5 sec in Stac Add Item as serverless is taking more time than other environments

--- geoportal/src/main/java/com/esri/geoportal/search/STACService.java
+++ geoportal/src/main/java/com/esri/geoportal/search/STACService.java
@@ -1315,7 +1315,14 @@ public class STACService extends Application {
             String itemFileString = this.readResourceFile(filePath, hsr);
 
             //Before searching newly added item, sleep for 1 second, otherwise record is not found
-            TimeUnit.SECONDS.sleep(1);
+            if(ec.getAwsOpenSearchType().equalsIgnoreCase("serverless"))
+            {
+            	TimeUnit.SECONDS.sleep(5);
+            }
+            else
+            {
+            	TimeUnit.SECONDS.sleep(1);
+            }            
 
             String itemRes = StacHelper.getItemWithItemId(collectionId, id);
             responseJSON = prepareResponseSingleItem(itemRes, itemFileString, collectionId);
