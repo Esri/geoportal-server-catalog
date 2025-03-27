91dd3f35373fd5af458ad77d970b9adca213ba07
cont_anki <ankita_srivastava@esri.com>
2025-03-27 11:12:40 +0530

    Updated for handling all opensearch requests

---
+++ geoportal/doc/aws-serverless/opensearch-lambda.py
@@ -0,0 +1,66 @@
+import boto3
+import json
+import requests
+from requests_aws4auth import AWS4Auth
+
+
+region = 'us-east-1' # For example, us-west-1
+service = 'aoss'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+# The OpenSearch domain endpoint with https:// and without a trailing slash
+#host = 'https://73ndg6pb0y39khhm7ij7.us-east-1.aoss.amazonaws.com'
+host = 'https://txjwwmxxavpt5fulgmxc.us-east-1.aoss.amazonaws.com'
+
+# Lambda execution starts here
+def lambda_handler(event, context):
+    print('event:', json.dumps(event))     
+
+    reqpath = event['path']    
+
+    if(len(reqpath) > 1):
+        url = host + reqpath        
+    else:
+        url = host
+
+    print("url with path",url)
+
+    method = event['httpMethod']
+    requestbody = event['body']
+    
+    reqBody ={}
+    
+    if requestbody:
+        reqBody.update(json.loads(requestbody))
+
+    queryParam = event['queryStringParameters']
+    if queryParam:       
+        index = url.find('point_in_time')
+        if(index > -1):            
+            keepAliveSec = queryParam['keep_alive']
+            url = url+'?keep_alive='+keepAliveSec           
+        else:               
+            reqBody.update(queryParam)
+   
+    print("body including queryparam",reqBody)
+  
+    # Elasticsearch 6.x requires an explicit Content-Type header
+    headers = { "Content-Type": "application/json" }
+
+    # Make the signed HTTP request
+    r = requests.request(method, url, auth=awsauth, headers=headers,data=json.dumps(reqBody))
+    print("response ",r)
+    # Create the response and add some extra content to support CORS
+    response = {
+        "statusCode": r.status_code,
+        "headers": {
+            "Access-Control-Allow-Origin": '*'
+        },
+        "isBase64Encoded": False
+    }
+
+    # Add the search results to the response
+    response['body'] = r.text
+    return response
+    
