bd9873c5dbeffe0f62637f34f4667897c047585b
cont_anki <ankita_srivastava@esri.com>
2025-07-28 13:02:14 +0530

    CodeQL issue 9

--- geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/GetItemRequest.java
+++ geoportal/src/main/java/com/esri/geoportal/lib/elastic/http/request/GetItemRequest.java
@@ -23,6 +23,8 @@ import com.esri.geoportal.lib.elastic.request.BulkEditRequest;
 import java.io.FileNotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.commons.text.StringEscapeUtils;
+
 
 /**
  * Get an item.
@@ -165,6 +167,8 @@ public class GetItemRequest extends BulkEditRequest {
    * @param json the item json
    */
   public void writeOk(AppResponse response, String json) {
+	 String encodedJson = StringEscapeUtils.escapeJson(json);
+	 response.setEntity(encodedJson);
     response.setEntity(json);
     response.setMediaType(MediaType.APPLICATION_JSON_TYPE.withCharset("UTF-8"));
     response.setStatus(Response.Status.OK);
